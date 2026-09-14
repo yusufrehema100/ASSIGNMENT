e73051e092a02ad3dd3c2e9b05bbbf47a294a7da
REHEMA <yusufrehema100@gmail.com>
2026-09-14 19:39:53 +0300

    Add files via upload

---
+++ question1.py
@@ -0,0 +1,53 @@
+class student:
+	pass
+
+student1= student()
+student1.student_id = "ST001"
+student1.name = "Yusuf"
+student1.age =22
+student1.gender = "Male"
+student1.programme = "Computer Science"
+student1.year_of_study = 1
+
+student2= student()
+student2.student_id = "ST002"
+student2.name = "Saidi"
+student2.age =23
+student2.gender = "Male"
+student2.programme = "Business Adiministration"
+student2.year_of_study = 3
+
+student3= student()
+student3.student_id = "ST003"
+student3.name = "Remmah"
+student3.age =21
+student3.gender = "Female"
+student3.programme = "Information Technology"
+student3.year_of_study = 2
+
+
+print("student 1")
+print("ID:", student1.student_id)
+print("Name:", student1.name)
+print("Age:", student1.age)
+print("Gender:", student1.gender)
+print("Programme:", student1.programme)
+print("Year_of_Study", student1.year_of_study)
+
+print("\nstudent 2")
+print("ID:", student2.student_id)
+print("Name:", student2.name)
+print("Age:", student2.age)
+print("Gender:", student2.gender)
+print("Programme:", student2.programme)
+print("Year_of_Study", student3.year_of_study)
+
+print("\nstudent 3")
+print("ID:", student3.student_id)
+print("Name:", student3.name)
+print("Age:", student3.age)
+print("Gender:", student3.gender)
+print("Programme:", student3.programme)
+print("Year_of_Study", student2.year_of_study)
+
+
